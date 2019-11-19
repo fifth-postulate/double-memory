36506519690da3c55229f510283640e86fa19653
Daan van Berkel <daan.van.berkel@alliander.com>
2019-11-19 17:40:04 +0100

    Show node and edge counts

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+extern crate matching;
+
+use matching::memory::graph;
+
 fn main() {
-    println!("Hello, world!");
+    for n in 1..11 {
+        let g = graph(n);
+        println!("n: {} v: {} e: {}", n, g.node_count(), g.edge_count());
 }
+    }
+
