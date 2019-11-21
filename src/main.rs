08edd4b384ee49c6253e019670d980a4ceb48172
Daan van Berkel <daan.van.berkel@alliander.com>
2019-11-21 07:22:47 +0100

    Correct construction of memory graph

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ extern crate matching;
 use matching::memory::graph;
 
 fn main() {
-    for n in 1..11 {
+    for n in 2..11 {
         let g = graph(n);
-        println!("n: {} v: {} e: {}", n, g.node_count(), g.edge_count());
-}
+        println!("n: {} v: {} e: {} c: {}", n, g.node_count(), g.edge_count(), n * (n-1) * (n-2)/2);
     }
-
+}
--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -4,12 +4,12 @@
 //! There will be an edge between two vertices (p,q) and (s,t) if and only if either of the numbers on the tiles agree. I.e. p equals s or t, or q equals s or t. E.g. Vertex (0,1) is connected by an edges with with vertex (0,2).
 
 use petgraph::{graph::Graph, Undirected};
-use std::collections::HashMap;
+use std::collections::BTreeMap as Dict;
 
 /// Create a memory graph
 pub fn graph(n: usize) -> Graph<(usize, usize), (), Undirected, u32> {
     let mut graph = Graph::with_capacity(n * (n-1)/2, 0);
-    let mut indices = HashMap::new();
+    let mut indices = Dict::new();
     for first in 0..n {
         for second in (first + 1)..n {
             let label = (first, second);
@@ -17,12 +17,12 @@ pub fn graph(n: usize) -> Graph<(usize, usize), (), Undirected, u32> {
             indices.insert(label, index);
         }
     }
-
+    
     for origin in indices.keys() {
         for neighbor in indices
             .keys()
+            .filter(|target| origin < target)
             .filter(|(p, q)| origin.0 == *p || origin.0 == *q || origin.1 == *p || origin.1 == *q)
-            .filter(|(p, q)| !(origin.0 == *p && origin.1 == *q))
         {
             let u_index = indices.get(origin).unwrap();
             let v_index = indices.get(neighbor).unwrap();
@@ -46,9 +46,9 @@ mod test {
     }
 
     #[test]
-    fn memory_10_graph_has_720() {
+    fn memory_10_graph_has_360_edges() {
         let g = graph(10);
 
-        assert_eq!(g.edge_count(), 720);
+        assert_eq!(g.edge_count(), 360);
     }
 }
