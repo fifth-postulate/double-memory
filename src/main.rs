bd207b5cfbf441d127b5c968a4fd49347305cd6f
Daan van Berkel <daan.van.berkel@alliander.com>
2019-11-21 07:23:25 +0100

    don't show calculated edge count

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ use matching::memory::graph;
 fn main() {
     for n in 2..11 {
         let g = graph(n);
-        println!("n: {} v: {} e: {} c: {}", n, g.node_count(), g.edge_count(), n * (n-1) * (n-2)/2);
+        println!("n: {} v: {} e: {}", n, g.node_count(), g.edge_count())
     }
 }
